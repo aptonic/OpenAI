9ef9e31345e1457fb8fe3ab5769842a1763515dd
aptonic <contact@aptonic.com>
2025-03-20 15:34:03 +1300

    Update ModelResult.swift

--- Sources/OpenAI/Public/Models/Models/ModelResult.swift
+++ Sources/OpenAI/Public/Models/Models/ModelResult.swift
@@ -9,23 +9,38 @@ import Foundation
 
 /// The model object matching the specified ID.
 public struct ModelResult: Codable, Equatable {
-
-    /// The model identifier, which can be referenced in the API endpoints.
     public let id: String
-    /// The Unix timestamp (in seconds) when the model was created.
     public let created: TimeInterval?
-    /// The object type, which is always "model".
     public let object: String?
-    /// The organization that owns the model.
     public let ownedBy: String?
-    /// Friendly name
     public let displayName: String?
+    public let supportsVision: Bool?  // New property
 
-    public enum CodingKeys: String, CodingKey {
-        case id
-        case created
-        case object
-        case ownedBy = "owned_by"
-        case displayName = "display_name"
+    enum CodingKeys: String, CodingKey {
+        case id, created, object, ownedBy = "owned_by", displayName = "display_name", modelSpec = "model_spec"
+    }
+    
+    enum ModelSpecCodingKeys: String, CodingKey {
+        case capabilities
+    }
+    
+    enum CapabilitiesCodingKeys: String, CodingKey {
+        case supportsVision
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        created = try? container.decode(TimeInterval.self, forKey: .created)
+        object = try? container.decode(String.self, forKey: .object)
+        ownedBy = try? container.decode(String.self, forKey: .ownedBy)
+        displayName = try? container.decode(String.self, forKey: .displayName)
+        
+        if let modelSpecContainer = try? container.nestedContainer(keyedBy: ModelSpecCodingKeys.self, forKey: .modelSpec),
+           let capabilitiesContainer = try? modelSpecContainer.nestedContainer(keyedBy: CapabilitiesCodingKeys.self, forKey: .capabilities) {
+            supportsVision = try? capabilitiesContainer.decode(Bool.self, forKey: .supportsVision)
+        } else {
+            supportsVision = nil
+        }
     }
 }
