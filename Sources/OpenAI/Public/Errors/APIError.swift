414586e814b7e403f264d4b96accb3c098f08560
aptonic <contact@aptonic.com>
2025-03-24 19:53:36 +1300

    Update APIError.swift

--- Sources/OpenAI/Public/Errors/APIError.swift
+++ Sources/OpenAI/Public/Errors/APIError.swift
@@ -1,6 +1,6 @@
 //
 //  APIError.swift
-//  
+//
 //
 //  Created by Sergii Kryvoblotskyi on 02/04/2023.
 //
@@ -12,19 +12,22 @@ public enum OpenAIError: Error {
 }
 
 public struct APIError: Error, Decodable, Equatable {
+    public let error: String?
     public let message: String
     public let type: String
     public let param: String?
     public let code: String?
   
-  public init(message: String, type: String, param: String?, code: String?) {
+    public init(message: String, error: String?, type: String, param: String?, code: String?) {
     self.message = message
     self.type = type
     self.param = param
     self.code = code
+    self.error = error
   }
   
   enum CodingKeys: CodingKey {
+    case error
     case message
     case type
     case param
@@ -32,11 +35,21 @@ public struct APIError: Error, Decodable, Equatable {
   }
   
   public init(from decoder: Decoder) throws {
+    // First try to decode as simple string error
+    if let container = try? decoder.singleValueContainer(),
+       let errorString = try? container.decode(String.self) {
+        self.message = errorString
+        self.type = "error"
+        self.error = errorString
+        self.param = nil
+        self.code = nil
+        return
+    }
+    
+    // If that fails, try the regular format
     let container = try decoder.container(keyedBy: CodingKeys.self)
     
-    //
-    // message can be String or [String].
-    //
+    // message can be String or [String]
     if let string = try? container.decode(String.self, forKey: .message) {
       self.message = string
     } else if let array = try? container.decode([String].self, forKey: .message) {
@@ -46,6 +59,7 @@ public struct APIError: Error, Decodable, Equatable {
     }
     
     self.type = try container.decode(String.self, forKey: .type)
+    self.error = try container.decodeIfPresent(String.self, forKey: .error)
     self.param = try container.decodeIfPresent(String.self, forKey: .param)
     self.code = try container.decodeIfPresent(String.self, forKey: .code)
   }
